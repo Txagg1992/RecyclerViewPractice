a2d873251f3a857d7fe2324066c8924b6e99aee7
txagg1992 <curiousaps@gmail.com>
2018-09-15 22:49:30 -0500

    Add Adapter

---
+++ app/src/main/java/com/curiousca/recyclerviewpractice/ExampleAdapter.java
@@ -0,0 +1,65 @@
+package com.curiousca.recyclerviewpractice;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ExampleAdapter extends RecyclerView.Adapter<ExampleAdapter.ExampleViewHolder> {
+
+    private Context mContext;
+    private ArrayList<ExampleItem> mExampleList;
+
+    public ExampleAdapter(Context context, ArrayList<ExampleItem> exampleList) {
+        this.mContext = context;
+        this.mExampleList = exampleList;
+    }
+
+    @NonNull
+    @Override
+    public ExampleViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.example_item, parent, false);
+        return new ExampleViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ExampleViewHolder holder, int position) {
+        ExampleItem currentItem = mExampleList.get(position);
+
+        String imageURL = currentItem.getImageURL();
+        String creatorName = currentItem.getCreator();
+        int likeCount = currentItem.getLikeCount();
+
+        holder.mTextViewCreator.setText(creatorName);
+        holder.mTextViewLikes.setText(likeCount);
+        Picasso.get().load(imageURL).fit().centerInside().into(holder.mImageView);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mExampleList.size();
+    }
+
+    public class ExampleViewHolder extends RecyclerView.ViewHolder{
+        public ImageView mImageView;
+        public TextView mTextViewCreator;
+        public TextView mTextViewLikes;
+
+        public ExampleViewHolder(View itemView) {
+            super(itemView);
+
+            mImageView = itemView.findViewById(R.id.image_view);
+            mTextViewCreator = itemView.findViewById(R.id.text_view_creator);
+            mTextViewLikes = itemView.findViewById(R.id.text_view_likes);
+        }
+    }
+}
